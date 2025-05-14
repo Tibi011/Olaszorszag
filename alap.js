2989f80937828046df31ece9fe4d3a8ccfcb5a42
Tibi011 <megsztil@gmail.com>
2025-05-14 12:08:30 +0200

    majdk

--- alap.js
+++ alap.js
@@ -12,3 +12,18 @@ function carousel() {
   x[myIndex-1].style.display = "block";  
   setTimeout(carousel, 5000);    
 }
+
+var myIndex2 = 0;
+carousel2();
+
+function carousel2() {
+  var i;
+  var x = document.getElementsByClassName("mySlides2");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex2++;
+  if (myIndex2 > x.length) {myIndex2 = 1}    
+  x[myIndex2-1].style.display = "block";  
+  setTimeout(carousel, 5000);    
+}
